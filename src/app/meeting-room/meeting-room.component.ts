af4a87b92a38ab29951df7e7434450cb70f9f749
JAVID ASHWAQ F <javidshahid19@gmail.com>
2024-01-11 16:43:07 +0530

    main

--- src/app/meeting-room/meeting-room.component.ts
+++ src/app/meeting-room/meeting-room.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -8,37 +8,44 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./meeting-room.component.scss']
 })
 
-
-export class MeetingRoomComponent {
-
+export class MeetingRoomComponent implements OnDestroy {
   @ViewChild("webCamVideo") webCamVideo !: ElementRef;
   @ViewChild("remoteVideo") remoteVideo !: ElementRef;
+  callsCollection:any;
   mediaDevice = navigator.mediaDevices;
   mediaStream: any;
   userName: string = sessionStorage["userName"];
+
+  isCameraOn = true;
+  isMikeOn = true
 
   constructor(private router: Router, private route: ActivatedRoute, private firestore: AngularFirestore ) {
     this.startCameraAndAudio()
   }
 
   endCall() {
-    console.log(this.route);
-    console.log(this.router);
     sessionStorage.clear();
     this.router.navigateByUrl("")
     this.stopCameraAndAudio()
   }
 
-  async startCall() {
-  };
 
-  async joinCall() {
+  camera() {
+    if(!this.isMikeOn) return this.stopCameraAndAudio()
+    this.isCameraOn = !this.isCameraOn
+  };
+  mike() {
+    if(!this.isCameraOn) return this.stopCameraAndAudio()
+    this.isMikeOn = !this.isMikeOn
+  }
+  joinCode() {
+    
   }
 
   startCameraAndAudio() {
     this.mediaDevice.getUserMedia({
-      video: true,
-      audio: true,
+      video: this.isCameraOn ,
+      audio: this.isMikeOn,
     })
       .then((stream) => { // Store the stream in the property
         console.log(stream);
@@ -50,6 +57,21 @@ export class MeetingRoomComponent {
         });
       })
       .catch((err) => alert(err));
+      this.callsCollection = this.firestore.collection("calls");
+
+      let callerId = '1234567890';
+      let calleeId = '0987654321';
+      this.callsCollection.add({
+        callerId: callerId, 
+        calleeId: calleeId, 
+      })
+      .then((docRef: any) => {
+        console.log( docRef)
+        console.log("Caller ID stored in Firestore with document ID: ", docRef.id)
+      })
+      .catch(function(error: any) {
+        console.error("Error adding document: ", error);
+       });
   }
   stopCameraAndAudio() { // Check if the mediaStream is defined
     if (this.mediaStream) {
@@ -63,6 +85,11 @@ export class MeetingRoomComponent {
       this.mediaStream = null;  // Set mediaStream to null
     }
   }
+
+  ngOnDestroy() {
+    this.endCall()
+  }
+
 
 
 }
